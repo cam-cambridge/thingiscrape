354c6aeb35408e10d7714ede0237d447fcbc26c5
Douglas Brion <me@douglasbrion.com>
2020-05-05 15:31:17 +0100

    Sorting objects by most popular in the last 7 days and printing out the items

--- scraper.py
+++ scraper.py
@@ -4,17 +4,22 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
 
-
-thingiverse_url = "https://www.thingiverse.com/"
-browser = webdriver.Chrome("./chromedrivers/chromedriver81_mac")
-
+thingiverse_url = 'https://www.thingiverse.com/'
+popular_sort_class = 'button[class*=Sort__dropdownButton--]'
+browser = webdriver.Chrome('./chromedrivers/chromedriver81_mac')
 def main():
     browser.get(thingiverse_url)
-    # home_page = requests.get(thingiverse_url)
+    dropdown = browser.find_elements_by_xpath('//button[starts-with(@class,"Sort__dropdownButton--")]')
+    if len(dropdown) == 1:
+        dropdown[0].click()
+        button = browser.find_elements_by_xpath('//span[contains(text(),"Popular last 7 Days")]')
+        button[0].click()
+    time.sleep(2)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     browser.close()
-    thing_card_header = soup.select('span[class*="ThingCardHeader__"]')
+    thing_card_header = soup.select('span[class*=ThingCardHeader__]')
     for thing in thing_card_header:
         print(thing.contents)
     browser.quit()
